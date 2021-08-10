475f913412b8748508a02606dc03a0d44fa68ff4
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-10 12:10:05 -0500

    added axios request and refactored

--- tvmaze.js
+++ tvmaze.js
@@ -7,8 +7,8 @@ BASEURL = 'http://api.tvmaze.com'
 
 /** Search Shows
  *    - given a search term, search for tv shows that
- *      match that query.  The function is async show it
- *       will be returning a promise.
+ *      match that query. The function is async show it
+ *      will be returning a promise.
  *
  *   - Returns an array of objects. Each object should include
  *     following show information:
@@ -20,19 +20,24 @@ BASEURL = 'http://api.tvmaze.com'
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
-
   await axios.get(`${BASEURL}/search/shows?q=${query}`)
-
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
+        .then(function (res) {
+          showsArray = []
+          for (const showObj of res.data) {
+            show = {
+              id: showObj.show.id,
+              name: showObj.show.name,
+              summary: showObj.show.summary,
+              image: showObj.show.image.medium
+            }
+            showsArray.push(show)
+          }
+          console.log(showsArray);
+          return showsArray
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
 }
 
 
@@ -76,7 +81,7 @@ $("#search-form").on("submit", async function handleSearch (evt) {
   $("#episodes-area").hide();
 
   let shows = await searchShows(query);
-
+  console.log(shows);
   populateShows(shows);
 });
 
